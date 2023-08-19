d5b0b9ab6d429fe5ee9b858b06a66b8185a992ef
Liaksej <tarusov.liaksej@gmail.com>
2023-08-19 18:45:14 +0200

    Initial commit

---
+++ src/ts/app.ts
@@ -0,0 +1,15 @@
+function app() {
+  const content = document.querySelector(".text-box-wrapper") as HTMLElement;
+  const wrapper = document.querySelector(".text-box-wrapper") as HTMLElement;
+  const collapseHandler = () => {
+    if (wrapper.style.maxHeight) {
+      wrapper.style.maxHeight = "";
+    } else {
+      wrapper.style.maxHeight = content?.scrollHeight + "px";
+    }
+  };
+
+  document.querySelector(".btn")?.addEventListener("click", collapseHandler);
+}
+
+app();
